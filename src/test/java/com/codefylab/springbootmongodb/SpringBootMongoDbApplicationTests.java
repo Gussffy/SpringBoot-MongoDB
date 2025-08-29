19ca7b19250b0ae382d810fdcd5d2a0eb9c29c1c
GustavoSantos <gsf34062@gmail.com>
2025-08-29 12:16:34 -0300

    Project Created

---
+++ src/test/java/com/codefylab/springbootmongodb/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefylab.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
